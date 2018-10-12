1c8dc9c82ade79737557870c297f0d0d08786a96
you liang <tan_you_liang@hotmail.com>
2018-10-12 11:04:33 +0800

    create new package

--- ur10_rmf/scripts/python_moveit.py
+++ ur10_rmf/scripts/python_moveit.py
@@ -1,247 +1,159 @@
 #!/usr/bin/env python
 
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, SRI International
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of SRI International nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Acorn Pooley
+# Open: 0, close: 1
 
-## BEGIN_SUB_TUTORIAL imports
-##
-## To use the python interface to move_group, import the moveit_commander
-## module.  We also import rospy and some messages that we will use.
 import sys
 import copy
 import rospy
+from time import sleep
+
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-## END_SUB_TUTORIAL
+import tf
+from arm_Manipulation import armManipulation
 
+from math import pi
 from std_msgs.msg import String
+from std_msgs.msg import Int32
+# from dynamixel_gripper.msg  import grip_state
+from ur10_rmf.msg import grip_state
 
-def move_group_python_interface_tutorial():
-  ## BEGIN_TUTORIAL
-  ##
-  ## Setup
-  ## ^^^^^
-  ## CALL_SUB_TUTORIAL imports
-  ##
-  ## First initialize moveit_commander and rospy.
-  print "============ Starting tutorial setup"
-  moveit_commander.roscpp_initialize(sys.argv)
-  rospy.init_node('move_group_python_interface_tutorial',
-                  anonymous=True)
-
-  ## the robot as a whole.
-  ## Instantiate a RobotCommander object.  This object is an interface to
-  robot = moveit_commander.RobotCommander()
-
-  ## Instantiate a PlanningSceneInterface object.  This object is an interface
-  ## to the world surrounding the robot.
-  scene = moveit_commander.PlanningSceneInterface()
-
-  ## Instantiate a MoveGroupCommander object.  This object is an interface
-  ## to one group of joints.  In this case the group is the joints in the left
-  ## arm.  This interface can be used to plan and execute motions on the left
-  ## arm.
-
-  print "Trying left arm >>>> \n"
-  group = moveit_commander.MoveGroupCommander("manipulator")
+from moveit_commander.conversions import pose_to_list
 
 
-  ## We create this DisplayTrajectory publisher which is used below to publish
-  ## trajectories for RVIZ to visualize.
-  display_trajectory_publisher = rospy.Publisher(
-                                      '/move_group/display_planned_path',
-                                      moveit_msgs.msg.DisplayTrajectory)
+rospy.init_node('ur10_motionPlanning', anonymous=True)
+pub = rospy.Publisher('gripper/command', Int32, queue_size=10)
 
-  ## Wait for RVIZ to initialize. This sleep is ONLY to allow Rviz to come up.
-  print "============ Waiting for RVIZ..."
-  rospy.sleep(2)
-  print "============ Starting tutorial "
+gripper_state = 0
+rate = rospy.Rate(5) # 5hz
 
-  ## Getting Basic Information
-  ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-  ##
-  ## We can get the name of the reference frame for this robot
-  print "============ Reference frame: %s" % group.get_planning_frame()
-
-  ## We can also print the name of the end-effector link for this group
-  print "============ Reference frame: %s" % group.get_end_effector_link()
-
-  ## We can get a list of all the groups in the robot
-  print "============ Robot Groups:"
-  print robot.get_group_names()
-
-  ## Sometimes for debugging it is useful to print the entire state of the
-  ## robot.
-  print "============ Printing robot state"
-  print robot.get_current_state()
-  print "============"
+enable_gripper = 1
 
 
-  # ## Planning to a Pose goal
-  # ## ^^^^^^^^^^^^^^^^^^^^^^^
-  # ## We can plan a motion for this group to a desired pose for the 
-  # ## end-effector
-  # print "============ Generating plan 1"
-  # pose_target = geometry_msgs.msg.Pose()
-  # pose_target.orientation.w = 1.0
-  # pose_target.position.x = 0.7
-  # pose_target.position.y = -0.05
-  # pose_target.position.z = 1.1
-  # group.set_pose_target(pose_target)
+def gripperState_callback(data): 
+  global gripper_state
+  # rospy.loginfo( "I heard gripper state is {}".format(data.gripper_state))
+  gripper_state = data.gripper_state
+    
+    
+def is_gripperOpen(): 
 
-  # ## Now, we call the planner to compute the plan
-  # ## and visualize it if successful
-  # ## Note that we are just planning, not asking move_group 
-  # ## to actually move the robot
-  # plan1 = group.plan()
+  if (enable_gripper == 1):
+    global gripper_state
+    gripper_command = 0       # command to open gripper
+    rospy.loginfo(gripper_command) #printout
+    pub.publish(gripper_command)
 
-  # print "============ Waiting while RVIZ displays plan1..."
-  # rospy.sleep(5)
-
- 
-  # ## You can ask RVIZ to visualize a plan (aka trajectory) for you.  But the
-  # ## group.plan() method does this automatically so this is not that useful
-  # ## here (it just displays the same trajectory again).
-  # print "============ Visualizing plan1"
-  # display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-
-  # display_trajectory.trajectory_start = robot.get_current_state()
-  # display_trajectory.trajectory.append(plan1)
-  # display_trajectory_publisher.publish(display_trajectory);   ##display again
-
-  # print "============ Waiting while plan1 is visualized (again)..."
-  # rospy.sleep(5)
+    # wait until get state is open
+    while (gripper_state == 1):
+      print "gripper still close... with state".format(gripper_state)
+      rate.sleep()
 
 
-  # ## Moving to a pose goal
-  # ## ^^^^^^^^^^^^^^^^^^^^^
-  # ##
-  # ## Moving to a pose goal is similar to the step above
-  # ## except we now use the go() function. Note that
-  # ## the pose goal we had set earlier is still active 
-  # ## and so the robot will try to move to that goal. We will
-  # ## not use that function in this tutorial since it is 
-  # ## a blocking function and requires a controller to be active
-  # ## and report success on execution of a trajectory.
+def is_gripperClose():
 
-  # # Uncomment below line when working with a real robot
-  # # group.go(wait=True)
+  if (enable_gripper == 1):
+    global gripper_state
+    gripper_command = 1       #command to close gripper
+    rospy.loginfo(gripper_command)
+    pub.publish(gripper_command)
 
-  # ## Planning to a joint-space goal 
-  # ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  # ##
-  # ## Let's set a joint space goal and move towards it. 
-  # ## First, we will clear the pose target we had just set.
-
-  # group.clear_pose_targets()
-
-  # ## Then, we will get the current set of joint values for the group
-  # group_variable_values = group.get_current_joint_values()
-  # print "============ Joint values: ", group_variable_values
-
-  # ## Now, let's modify one of the joints, plan to the new joint
-  # ## space goal and visualize the plan
-  # group_variable_values[0] = 1.0
-  # group.set_joint_value_target(group_variable_values)
-
-  # plan2 = group.plan()
-
-  # =================================================================================================================================
-
-  print "============ Waiting while RVIZ displays plan2..."
-  print "  Cartesian!!"
-  rospy.sleep(5)
+    # wait until get state is close
+    while (gripper_state == 0):
+      print "gripper still open... with state {} ".format(gripper_state)
+      rate.sleep()
 
 
-  ## Cartesian Paths
-  ## ^^^^^^^^^^^^^^^
-  ## You can plan a cartesian path directly by specifying a list of waypoints 
-  ## for the end-effector to go through.
-  waypoints = []
+def gripper_InOut_payLoad():
 
-  # start with the current pose
-  waypoints.append(group.get_current_pose().pose)
+  print "============ Press `Enter` to put hand into payload ..."
+  raw_input()
+  cartesian_plan, planned_fraction = ur10.plan_cartesian_payload_path(scale=2)
+  print " # Planned fraction: {} ".format(planned_fraction)
+    
+  if (planned_fraction == 1.0):
+    ur10.execute_plan(cartesian_plan)
 
-  # first orient gripper and move forward (-x)
-  wpose = geometry_msgs.msg.Pose()
-  # wpose.orientation.w = 1.0
-  wpose.position.x = waypoints[0].position.x - 0.1
-  wpose.position.y = waypoints[0].position.y
-  wpose.position.z = waypoints[0].position.z
-  waypoints.append(copy.deepcopy(wpose))
+  is_gripperOpen()
 
-  # second move down
-  wpose.position.z -= 0.50
-  waypoints.append(copy.deepcopy(wpose))
+  print "============ Press `Enter` to detach and remove the box from the Panda robot n scene..."
+  raw_input()
+  ur10.detach_box()
+  ur10.remove_box()
 
-  # third move to the side
-  wpose.position.y -= 0.5
-  waypoints.append(copy.deepcopy(wpose))
-
-  ## We want the cartesian path to be interpolated at a resolution of 1 cm
-  ## which is why we will specify 0.01 as the eef_step in cartesian
-  ## translation.  We will specify the jump threshold as 0.0, effectively
-  ## disabling it.
-  (plan3, fraction) = group.compute_cartesian_path(
-                               waypoints,   # waypoints to follow
-                               0.01,        # eef_step
-                               0.0)         # jump_threshold
-                               
-  print "============ Waiting while RVIZ displays Cartesian plan3..."
-  rospy.sleep(5)
-
- 
-  ## Adding/Removing Objects and Attaching/Detaching Objects
-  ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  ## First, we will define the collision object message
-  collision_object = moveit_msgs.msg.CollisionObject()
+  print "============ Press `Enter` to put hand out of payload ..."
+  raw_input()
+  cartesian_plan, planned_fraction = ur10.plan_cartesian_payload_path(scale=-2)
+  print " # Planned fraction: {} ".format(planned_fraction)
+  
+  if (planned_fraction == 1.0):
+    ur10.execute_plan(cartesian_plan)
 
 
 
-  ## When finished shut down moveit_commander.
-  moveit_commander.roscpp_shutdown()
-
-  ## END_TUTORIAL
-
-  print "============ STOPPING"
+## =============================================== MAIN ==============================================================
 
 
-if __name__=='__main__':
+def main():
+
+  rospy.Subscriber("gripper/state", grip_state, gripperState_callback)
+
   try:
-    print "yo starts!!!! \n\n"
-    move_group_python_interface_tutorial()
-  except rospy.ROSInterruptException:
-    pass
 
+    print "============ Press `Enter` to execute a movement to place beverage joint state goal ..."
+    raw_input()
+    place_joint_goal = [0, -pi/4, pi/2, -pi/4, pi/2, -pi/4]
+    print " # Success? ", ur10.go_to_joint_state(place_joint_goal)
+    is_gripperOpen()
+
+    print "============ Press `Enter` to execute a movement to rest joint state goal ..."
+    raw_input()
+    rest_joint_goal = [0, -2*pi/5, pi/2+0.2, -pi/4 - 2*pi/5 + pi/2, pi/2, -pi/4]
+    print " # Success? ", ur10.go_to_joint_state(rest_joint_goal)
+    
+    print "============ Press `Enter` to plan and display a Cartesian path ..."
+    raw_input()
+    cartesian_plan, planned_fraction = ur10.plan_cartesian_picking_path(scale=2)
+    print " # Planned fraction: {} ".format(planned_fraction)
+
+    if (planned_fraction == 1.0):
+      ur10.execute_plan(cartesian_plan)
+
+    print "============ Press `Enter` to add obj to scene and attach to robot ..."
+    raw_input()
+    ur10.add_box()
+    ur10.attach_box()
+
+    # Grip beveerage on rack
+    is_gripperClose()
+
+    print "============ Press `Enter` to plan and execute a path with an attached collision object ..."
+    raw_input()
+    cartesian_plan, planned_fraction = ur10.plan_cartesian_placing_path(scale=2)
+    print " # Planned fraction: {} ".format(planned_fraction)
+
+    if (planned_fraction == 1.0):
+      print ur10.execute_plan(cartesian_plan)
+
+    print "============ Press `Enter` to execute a movement to place beverage joint state goal ..."
+    raw_input()
+    place_joint_goal = [0, -pi/4, pi/2, -pi/4, pi/2, -pi/4]
+    print " # Success? ", ur10.go_to_joint_state(place_joint_goal)
+
+    gripper_InOut_payLoad()
+
+    print "============ Python demo complete! ========="
+
+
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
+
+
+if __name__ == '__main__':
+  
+  print "============ Begin the python moveit script by setting up the moveit_commander (press ctrl-d to exit) ..."
+  ur10 = armManipulation()   ## moveGroup  
+  main()
